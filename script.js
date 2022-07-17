ce6050a7dc2799616d67ab2124fddf83b456928b
Antanas Pužas <antanaspuzas@protonmail.com>
2022-07-17 17:39:38 +0300

    Add user input storage in array of objects

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let myLibrary = [];
+let nameInput = document.querySelector("#book-title");
+let titleInput = document.querySelector("#book-author");
+let pagesInput = document.querySelector("#pages");
+let readInput = document.querySelector("#read");
+let submitInput = document.querySelector("#submit");
 
 function Book(title, author, pages, read) {
     this.title = title,
         this.author = author,
         this.pages = pages,
-        this.read = read,
-
-        this.info = () => title + " by " + author + ", " + pages + ", " +
-            (read ? "have read" : "not read yet")
+        this.read = read
 }
+
+Book.prototype.info = () => title + " by " + author + ", " + pages + ", " +
+(read ? "have read" : "not read yet");
 
 function addBookToLibrary() {
-
+    myLibrary.push(new Book(nameInput.value, titleInput.value,
+        pagesInput.value, readInput.checked));
+    console.log(myLibrary);
 }
 
-let book = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
-console.log(book.info());
+submitInput.addEventListener("click", () => {
+    addBookToLibrary();
+});
+
+// TEMP
+for (let i = 0; i < 3; i++) {
+    myLibrary.push(new Book("title-" + i, "author-" + i, Math.floor(Math.random() * (1000 - 100) + 100), true));
+}
+console.log(myLibrary);
